c97d9e91fffa82581058029eb43c67769fe8dd86
Joao P Dubas <joao.dubas@gmail.com>
2018-05-04 07:27:14 -0300

    chore(async): add chord tasks.

--- async/svc/tasks.py
+++ async/svc/tasks.py
@@ -1,9 +1,42 @@
 from __future__ import absolute_import, unicode_literals
+import datetime
 
 from celery import chain, chord, group
 
 from .celery import app
 
+
+DATE_FORMAT = '%Y-%d-%m'
+
+
+@app.task
+def process_report(start_date, end_date, email):
+    start = datetime.datetime.strptime(start_date, DATE_FORMAT)
+    end = datetime.datetime.strptime(end_date, DATE_FORMAT)
+    tasks = []
+    curr = start
+    while curr <= end:
+        tasks.append(process_line.s(curr.strftime(DATE_FORMAT)))
+        curr += datetime.timedelta(days=1)
+    return chord(tasks, report.s(email))()
+
+
+@app.task
+def report(lines, email):
+    return lines, email
+
+
+@app.task
+def process_line(dt):
+    return chord(
+        [
+            signup_by_date.s(dt),
+            approval_by_date.s(dt),
+            denial_by_date.s(dt),
+            failure_by_date.s(dt),
+        ],
+        line_for_date.s(dt)
+    )()
 
 @app.task
 def line_for_date(cols, dt):
@@ -12,19 +45,19 @@ def line_for_date(cols, dt):
 
 @app.task
 def signup_by_date(dt):
-    return 1
+    return {'count': 1, 'value': 1, 'step': 'signup'}
 
 
 @app.task
 def approval_by_date(dt):
-    return 1
+    return {'count': 1, 'value': 1, 'step': 'approval'}
 
 
 @app.task
 def denial_by_date(dt):
-    return 1
+    return {'count': 1, 'value': 1, 'step': 'denial'}
 
 
 @app.task
 def failure_by_date(dt):
-    return 1
+    return {'count': 1, 'value': 1, 'step': 'failure'}
