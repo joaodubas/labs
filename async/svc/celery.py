baca1b7f84c0fca596870da88f171c33a17e8ded
Joao P Dubas <joao.dubas@gmail.com>
2018-05-05 11:56:42 -0300

    feat(async): configure app logging.

--- async/svc/celery.py
+++ async/svc/celery.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import, unicode_literals
+import logging
+
 from celery import Celery
 
 app = Celery(
@@ -8,5 +10,25 @@ app = Celery(
     include=['svc.tasks']
 )
 
+
+def logger():
+    # NOTE: get logger
+    l = logging.getLogger('tasks')
+    l.setLevel(logging.INFO)
+
+    # NOTE: create handler
+    s = logging.StreamHandler()
+    s.setLevel(logging.INFO)
+
+    # NOTE: define log format
+    f = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+    s.setFormatter(f)
+
+    # NOTE: define handler for logger
+    l.setHandler(s)
+
+    return l
+
 if __name__ == '__main__':
+    logger()
     app.start()
