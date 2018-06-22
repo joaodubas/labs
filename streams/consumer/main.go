31e443924dd515a0ae2d65afab1b3b11ff482e7d
Joao Paulo Dubas <joao.dubas@gmail.com>
2018-06-22 16:09:15 -0300

    feat(streams): ack message after consume it.
    Also improve comments and notes.

--- streams/consumer/main.go
+++ streams/consumer/main.go
@@ -61,28 +61,53 @@ func recv(c redis.Conn, sID string) {
 	if err != nil {
 		log.Printf("recv: failed to read for group %v", err)
 	} else {
-		// TODO (jpd): convert this into a method to unmarshal redis stream message
+		// TODO (jpd): convert this in a method to parse redis stream message.
+		// A message is represented by a slice of streams, containing a slice
+		// of messages. Each message is a slice with a message id and a slice
+		// of key values. Example: `[[stream [[id [[k] [v]]]]]]`
 		for _, m := range r {
 			for i, n := range m.([]interface{}) {
 				if i == 0 {
-					log.Printf("recv: received stream (%d) %v", len(n.([]uint8)), string(n.([]uint8)))
+					log.Printf(
+						"recv: received stream (%d) %v",
+						len(n.([]uint8)),
+						string(n.([]uint8)),
+					)
 				} else {
 					for _, o := range n.([]interface{}) {
 						var key string
+						// NOTE (jpd): from here on we have a message.
 						for j, p := range o.([]interface{}) {
 							if j == 0 {
 								key = p.(string)
-								log.Printf("recv: received key (%d) %s", len(p.(string)), p.(string))
+								log.Printf(
+									"recv: received key (%d) %s",
+									len(p.(string)),
+									p.(string),
+								)
 							} else {
 								for _, q := range p.([]interface{}) {
-									log.Printf("recv: received message (%d) %s", len(q.([]uint8)), string(q.([]uint8)))
+									log.Printf(
+										"recv: received message (%d) %s",
+										len(q.([]uint8)),
+										string(q.([]uint8)),
+									)
 								}
 							}
 						}
-						log.Printf("should ack %s", key)
+						// NOTE (jpd): after process a message, ack it.
+						ack(c, key)
 					}
 				}
 			}
 		}
+	}
+}
+
+func ack(c redis.Conn, messageID string) {
+	if r, err := c.Do("XACK", StreamName, GroupName, messageID); err != nil {
+		log.Printf("ack: failed to ack message %s", messageID)
+	} else {
+		log.Printf("ack: success to ack message %s (%v)", messageID, r)
 	}
 }
