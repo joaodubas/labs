766ff6625e4df1475c4454fcf09bd6371268eaba
Joao P Dubas <joao.dubas@gmail.com>
2019-08-13 16:30:51 +0000

    chore(ide) unpin versions

--- ide/scripts/install.extensions.sh
+++ ide/scripts/install.extensions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 code-server \
-  --install-extension eamodio.gitlens@9.9.3 \
-  --install-extension EditorConfig.EditorConfig@0.13.0 \
-  --install-extension formulahendry.docker-explorer@0.1.6 \
-  --install-extension GitHub.vscode-pull-request-github@0.9.1 \
-  --install-extension mjmcloug.vscode-elixir@1.1.0 \
-  --install-extension ms-python.python@2019.8.29288 \
-  --install-extension octref.vetur@0.22.0 \
-  --install-extension PeterJausovec.vscode-docker@0.6.2 \
-  --install-extension Prisma.vscode-graphql@0.2.1 \
-  --install-extension vscodevim.vim@1.9.0
+  --install-extension eamodio.gitlens \
+  --install-extension EditorConfig.EditorConfig \
+  --install-extension formulahendry.docker-explorer \
+  --install-extension GitHub.vscode-pull-request-github \
+  --install-extension mjmcloug.vscode-elixir \
+  --install-extension ms-python.python \
+  --install-extension octref.vetur \
+  --install-extension PeterJausovec.vscode-docker \
+  --install-extension Prisma.vscode-graphql \
+  --install-extension vscodevim.vim
