50234c0cea447e701c0a04607c5677170ab58200
Joao P Dubas <joao.dubas@gmail.com>
2019-12-10 11:00:02 +0000

    chore(db) fix database setup.

--- db-replica/scripts/cockroachdb/setup.sh
+++ db-replica/scripts/cockroachdb/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export PATH=/cockroach:$PATH
-cockroach user set maxroach --insecure
-cockroach sql --insecure -e 'CREATE DATABASE IF NOT EXISTS bank'
-cockroach sql --insecure -e 'GRANT ALL ON DATABASE bank TO maxroach'
+cockroach user set maxroach --certs-dir /certs --host db_one
+cockroach sql --certs-dir /certs --host db_one -e 'CREATE DATABASE IF NOT EXISTS bank'
+cockroach sql --certs-dir /certs --host db_one -e 'GRANT ALL ON DATABASE bank TO maxroach'
