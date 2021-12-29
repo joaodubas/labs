f9651ba0554d9aa4dee2ebe4d217c655538d0e8f
Joao P Dubas <joao.dubas@gmail.com>
2021-12-29 09:08:34 -0300

    feat(ide) create global tool versions
    This should make easier to install global interpreters/plugins.

--- ide/scripts/docker-entrypoint.sh
+++ ide/scripts/docker-entrypoint.sh
@@ -2,8 +2,13 @@
 set -e
 
 if [ "$1" = 'code-server' ] || [ "$1" = 'openvscode-server' ]; then
+    echo 'change ownership from coder home'
     chown --recursive coder:coder /home/coder
+
+    echo 'create zsh cache path'
     gosu coder mkdir -p /home/coder/.cache/zsh
+
+    echo 'execute command'
     exec gosu coder "$@"
 fi
 
