addc1b3af04be79674b1e313582b4aa660491a97
Joao P Dubas <joao.dubas@gmail.com>
2021-12-29 09:11:17 -0300

    feat(ide) migrate neovim config to lua
    Also setup some cool modern plugins.

---
+++ ide/config/lua-nvim/lua/joaodubas/lsp/lsp-installer.lua
@@ -0,0 +1,23 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+  return
+end
+
+-- Register a handler that will be called for all installed server.
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    on_attach = require("joaodubas.lsp.handlers").on_attach,
+    capabilities = require("joaodubas.lsp.handlers").capabilities,
+  }
+
+  local server_opts = { }
+  if server.name == "jsonls" then
+    server_opts = require("joaodubas.lsp.settings.jsonls")
+  elseif server.name == "sumneko_lua" then
+    server_opts = require("joaodubas.lsp.settings.sumneko_lua")
+  end
+
+  opts = vim.tbl_deep_extend("force", server_opts, opts)
+
+  server:setup(opts)
+end)
