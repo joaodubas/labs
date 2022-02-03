2c1c3a8fb19c7d9542e9e49f5a3f4782bba2548b
Joao P Dubas <joao.dubas@gmail.com>
2022-02-03 12:34:35 +0000

    chore (ide) adjust entrypoint to create ext dir
    To install extensions in coder and openvscode the extensions dir must be
    created ahead of time.

--- ide/scripts/docker-entrypoint.sh
+++ ide/scripts/docker-entrypoint.sh
@@ -8,6 +8,10 @@ if [ "$1" = 'code-server' ] || [ "$1" = 'openvscode-server' ]; then
     echo 'create zsh cache path'
     gosu coder mkdir -p /home/coder/.cache/zsh
 
+    echo 'create coder/openvscode extension dir'
+    gosu coder mkdir -p /home/coder/.cache/code-server
+    gosu coder mkdir -p /home/coder/.cache/openvscode/extensions
+
     echo 'execute command'
     exec gosu coder "$@"
 fi
