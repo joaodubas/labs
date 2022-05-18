ba1d2a84ceefd46c0b669b1b8e02edea9932e607
Joao P Dubas <joao.dubas@gmail.com>
2022-05-18 11:07:34 +0000

    chore (nvim): add CMD global
    This function allow one to interact with the underlying system.

--- ide/config/nvim/lua/joaodubas/globals.lua
+++ ide/config/nvim/lua/joaodubas/globals.lua
@@ -1,5 +1,20 @@
 -- based on tj code available in:
 -- https://github.com/tjdevries/config_manager/blob/5d7d5b98fccd069829606464e65603017822cd72/xdg_config/nvim/lua/tj/globals.lua
+job = require("plenary.job")
+
+CMD = function(cmd, args)
+  local output
+  local p = job:new({
+    command = cmd,
+    args = args,
+    on_exit = function(j, r)
+      output = r == 0 and j:result() or {}
+      print(vim.inspect(output))
+    end,
+  })
+  p:sync()
+  return output
+end
 
 P = function(v)
   print(vim.inspect(v))
