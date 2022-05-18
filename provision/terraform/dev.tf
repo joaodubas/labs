0ea72b6f1d750170c466a53a6608fc8d298bfd28
Joao P Dubas <joao.dubas@gmail.com>
2022-05-18 15:04:05 +0000

    wip (provision)
    1. Add notes to playbooks that should be reviewed
    2. Configure grafana subdomain

--- provision/terraform/dev.tf
+++ provision/terraform/dev.tf
@@ -135,6 +135,13 @@ resource "digitalocean_record" "dev_minio_console" {
   value = digitalocean_droplet.dev_server.ipv4_address
 }
 
+resource "digitalocean_record" "dev_grafana" {
+  domain = digitalocean_domain.dev_default.name
+  type = "A"
+  name = "grafana"
+  value = digitalocean_droplet.dev_server.ipv4_address
+}
+
 resource "digitalocean_record" "simplelogin_mx_1" {
   domain = digitalocean_domain.dev_default.name
   type = "MX"
