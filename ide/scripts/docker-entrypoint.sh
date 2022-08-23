44646e484ee39cf2d42b7ca109219c14d37dd154
Joao P Dubas <joao.dubas@gmail.com>
2022-08-23 15:04:29 -0300

    feat(ide): upgrade coder/openvscode (#229)
    * coder from 4.5.0 to 4.6.0
    * openvscode from 1.68.1 to 1.70.1
    
    Also add some comments to ide entrypoint script.

--- ide/scripts/docker-entrypoint.sh
+++ ide/scripts/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# TODO (jpd): configure pyenv and git links when proper env vars are set
+# git config --global user.name 'Joao P Dubas'
+# git config --global user.email joao.dubas@gmail.com
+# git config --global core.editor nvim
+# pyenv global 3.10.4
+# ln -s ${HOME}/.local/share/pypoetry/venv/bin/poetry ${HOME}/.local/bin/poetry
+
 if [ "$1" = 'code-server' ] || [ "$1" = 'openvscode-server' ]; then
     echo 'change ownership from coder home'
     chown --recursive coder:coder /home/coder
