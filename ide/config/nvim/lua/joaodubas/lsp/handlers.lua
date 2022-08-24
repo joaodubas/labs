1f28f0024770cb76c0702139779e1ad1183f5867
Joao P Dubas <joao.dubas@gmail.com>
2022-08-24 17:17:19 +0000

    chore(nvim): migrate lsp.diagnostic deprecated functions

--- ide/config/nvim/lua/joaodubas/lsp/handlers.lua
+++ ide/config/nvim/lua/joaodubas/lsp/handlers.lua
@@ -69,9 +69,9 @@ local function lsp_keymaps(bufnr)
   keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
   keymap(bufnr, "n", "<C-K>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
   keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-  keymap(bufnr, "n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev({ border = \"rounded\" })<cr>", opts)
-  keymap(bufnr, "n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next({ border = \"rounded\" })<cr>", opts)
-  keymap(bufnr, "n", "gl", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostic({ border = \"rounded\" })<cr>", opts)
+  keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+  keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+  keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
   keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<cr>", opts)
 
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
