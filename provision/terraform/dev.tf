61b94bb86f0e8a1944b1d4f823eba28ba42f7813
Joao P Dubas <joao.dubas@gmail.com>
2024-02-01 14:56:01 +0000

    feat(terraform): configure firewall

--- provision/terraform/dev.tf
+++ provision/terraform/dev.tf
@@ -39,6 +39,46 @@ resource "digitalocean_droplet" "dev_server" {
   }
 }
 
+resource "digitalocean_firewall" "dev_firewall" {
+  name = "dev-ssh-http-https"
+  droplet_ids = [
+    digitalocean_droplet.dev_server.id
+  ]
+  tags = [
+    digitalocean_tag.dev_tag.name
+  ]
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "222"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol = "udp"
+    port_range = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
 resource "digitalocean_domain" "dev_default" {
   name = "dubas.dev"
   ip_address = digitalocean_droplet.dev_server.ipv4_address
