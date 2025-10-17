b29202a4e31a55465117193d670e2e139e52fcea
Joao P Dubas <joao.dubas@gmail.com>
2025-10-17 09:21:43 -0300

    fix(comtrya): issues in fish config file
    * Remove curly braces from env vars
    * Remove `-k` flag from bind statement, generated by `atuin init fish`
      * This is fixed in [PR atuin/2902](https://github.com/atuinsh/atuin/pull/2902)

--- provision/comtrya/user/files/fish/config.fish
+++ provision/comtrya/user/files/fish/config.fish
@@ -1,7 +1,8 @@
-{$LOCAL_BIN_HOME}/starship init fish | source
-{$LOCAL_BIN_HOME}/mise activate fish | source
-{$LOCAL_BIN_HOME}/zoxide init fish | source
-{$ATUIN_BIN}/atuin init fish | source
+$LOCAL_BIN_HOME/starship init fish | source
+$LOCAL_BIN_HOME/mise activate fish | source
+$LOCAL_BIN_HOME/zoxide init fish | source
+# NOTE: (jpd) Since the `-k` flag is no longer supported in `bind`, I'm removing it manually.
+$ATUIN_BIN/atuin init fish | sed 's/-k up/up/g' | source
 alias cat="bat"
 alias l="eza --time-style=long-iso --color=auto --classify=always"
 alias ll="l -ahl"
