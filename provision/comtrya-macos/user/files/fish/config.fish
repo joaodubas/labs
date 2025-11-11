cfd9173c611393d06a7c54b4e4ade2e56fa9785a
Joao P Dubas <joao.dubas@gmail.com>
2025-11-11 08:52:56 -0300

    chore(provision): set proper configuration for fish

---
+++ provision/comtrya-macos/user/files/fish/config.fish
@@ -0,0 +1,9 @@
+/opt/homebrew/bin/starship init fish | source
+/opt/homebrew/bin/mise activate fish | source
+/opt/homebrew/bin/zoxide init fish | source
+/opt/homebrew/bin/atuin init fish | source
+eval "$(/opt/homebrew/bin/brew shellenv)"
+alias cat="bat"
+alias l="eza --time-style=long-iso --color=auto --classify=always"
+alias ll="l -ahl"
+alias la="l -a"
