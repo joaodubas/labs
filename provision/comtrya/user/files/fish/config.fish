1bf2e92f9deec18faad9890848c49ac90323bcba
Joao P Dubas <joao.dubas@gmail.com>
2025-12-09 10:07:22 -0300

    chore(fish): add missing aliases

--- provision/comtrya/user/files/fish/config.fish
+++ provision/comtrya/user/files/fish/config.fish
@@ -10,3 +10,9 @@ alias la="l -a"
 alias k="kubectl"
 alias dc="docker compose"
 alias nh="nvim --listen 0.0.0.0:6666 --headless &> /dev/null"
+# Explanation:
+# - `mise outdated --bump --json`: This part generates the JSON output of outdated dependencies.
+# - `jq -r '...'`: jq is used to process JSON data. The -r flag outputs raw strings without JSON quoting.
+# -` map("\(.name)@\(.latest)")`: This iterates over each object in the JSON array. For each object, it constructs a string in the format "name@version" using the name and latest fields.
+# - `join(" ")`: This takes the array of "name@version" strings and joins them into a single string, with each item separated by a space.
+alias mise_up="mise outdated --bump --json | jq -r 'map(\"\(.name)@\(.latest)\") | join(\" \")'"
