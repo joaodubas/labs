536a9e7357716167ae53ac9013aadff89e054fdf
Joao P Dubas <joao.dubas@gmail.com>
2026-01-06 15:57:54 -0300

    chore(provision): update tools and enhance shell configurations (#383)

---
+++ provision/comtrya/system-deps/files/onepassword/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+# NOTE: (jpd) Based on https://developer.1password.com/docs/cli/get-started/
+echo "*** Configure signature and repository for 1Password CLI ***"
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | sudo tee /etc/apt/sources.list.d/1password.list
+echo "*** ***"
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+echo "*** ***"
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+echo "*** Install 1Password CLI ***"
+sudo apt-get update && sudo apt-get install 1password-cli
--- provision/comtrya/user/files/fish/config.fish
+++ provision/comtrya/user/files/fish/config.fish
@@ -10,3 +10,32 @@ alias la="l -a"
 alias k="kubectl"
 alias dc="docker compose"
 alias nh="nvim --listen 0.0.0.0:6666 --headless &> /dev/null"
+# Explanation:
+# - `mise outdated --bump --json`: This part generates the JSON output of outdated dependencies.
+# - `jq -r '...'`: jq is used to process JSON data. The -r flag outputs raw strings without JSON quoting.
+# -` map("\(.name)@\(.latest)")`: This iterates over each object in the JSON array. For each object, it constructs a string in the format "name@version" using the name and latest fields.
+# - `join(" ")`: This takes the array of "name@version" strings and joins them into a single string, with each item separated by a space.
+alias mise_up="mise outdated --bump --json | jq -r 'map(\"\(.name)@\(.latest)\") | join(\" \")'"
+
+function sesh-sessions
+    # Run sesh list and pipe to gum to allow interactive selection.
+    # The output of gum (the selected session) is captured into the 'session' variable.
+    set -l session (sesh list -i | gum filter --limit 1 --no-strip-ansi --no-sort --fuzzy --placeholder 'Pick a sesh' --height 10 --prompt='⚡')
+
+    # Repaint the command line to clear any fzf output artifacts.
+    commandline -f repaint
+
+    # If a session was selected (i.e., the 'session' variable is not empty), connect to it.
+    if test -n "$session"
+        sesh connect $session
+    end
+end
+
+function fish_user_key_bindings
+    # Insert mode binding
+    bind -M insert \es sesh-sessions
+    # Default (command) mode binding
+    bind -M default \es sesh-sessions
+    # Optionally, for visual mode if you use it extensively
+    # bind -M visual \es sesh-sessions
+end
