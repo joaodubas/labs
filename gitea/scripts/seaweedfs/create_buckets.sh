67f69119c3f4b6419ddd260566384b9c5c6cd263
Joao P Dubas <joao.dubas@gmail.com>
2026-02-02 08:53:06 -0300

    fix(seaweedfs): typo in command and master arguments

--- gitea/scripts/seaweedfs/create_buckets.sh
+++ gitea/scripts/seaweedfs/create_buckets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Wait for SeaweedFS Primary to be ready
-until weed shell -master=seaweedfs:9333 -comand "cluster.check" > /dev/null 2>&1; do
+until weed shell -master=seaweedfs:9333 -command "cluster.check" > /dev/null 2>&1; do
 	echo "Waiting for SeaweedFS Primary..."
 	sleep 2
 done
@@ -10,6 +10,6 @@ done
 echo "Creating buckets..."
 for bucket in forgejo k3d-registry; do
 	echo "Create bucket $bucket"
-	echo "s3.bucket.create -name=$bucket" | weed shell -mster=seaweedfs:9333
+	echo "s3.bucket.create -name=$bucket" | weed shell -master=seaweedfs:9333
 done
 echo "Buckets created successfully."
