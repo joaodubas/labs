3aa48e5f102b088907385083d56fe50648e1a60f
Joao P Dubas <joao.dubas@gmail.com>
2026-08-04 10:08:58 -0300

    feat(provision): add bogglex a record (#431)

--- provision/terraform/dev.tf
+++ provision/terraform/dev.tf
@@ -105,6 +105,13 @@ resource "digitalocean_record" "dev_bitwarden" {
   value = digitalocean_droplet.dev_server.ipv4_address
 }
 
+resource "digitalocean_record" "dev_bogglex" {
+  domain = digitalocean_domain.dev_default.name
+  type = "A"
+  name = "bogglex"
+  value = digitalocean_droplet.dev_server.ipv4_address
+}
+
 resource "digitalocean_record" "dev_crescer" {
   domain = digitalocean_domain.dev_default.name
   type = "A"
